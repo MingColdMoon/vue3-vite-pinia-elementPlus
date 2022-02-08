ead48780a7b68259248b49929f127cbf02831c1c
xiaotangguyu <xiaotangguyu@xiaotangguyu>
2022-02-08 19:00:16 +0800

    1

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import router from './route'
+import router from './route/Index'
 import { createPinia } from 'pinia'
 const pinia = createPinia()
 
